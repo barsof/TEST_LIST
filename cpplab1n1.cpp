880308442db18afcb5c31cad51722d3ff3688165
unknown <fffosrab@gmail.com>
2020-06-09 14:15:42 +0300

    load a file

---
+++ cpplab1n1.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "stdafx.h"
+#include "iostream" 
+#include "ctime" 
+#include "time.h"
+#include "windows.h"
+
+
+
+using namespace std;
+
+
+
+void ConvertTimeTToSystemTime(PSYSTEMTIME st, time_t* tt) {
+	tm my_tm;
+	localtime_s(&my_tm, tt);
+	memset(st, 0, sizeof(SYSTEMTIME));
+	st->wYear = my_tm.tm_year + 1900;
+	st->wMonth = my_tm.tm_mon + 1;
+	st->wDay = my_tm.tm_mday;
+	st->wHour = my_tm.tm_hour;
+	st->wMinute = my_tm.tm_min;
+	st->wSecond = my_tm.tm_sec;
+}
+
+
+int main()
+{
+	int start = clock();
+	struct tm {
+		int tm_sec; /* seconds after the minute - [0,59] */
+		int tm_min; /* minutes after the hour - [0,59] */
+		int tm_hour; /* hours since midnight - [0,23] */
+		int tm_mday; /* day of the month - [1,31] */
+		int tm_mon; /* months since January - [0,11] */
+		int tm_year; /* years since 1900 */
+		int tm_wday; /* days since Sunday - [0,6] */
+		int tm_yday; /* days since January 1 - [0,365] */
+		int tm_isdst; /* daylight savings time flag */
+	};
+
+
+	SYSTEMTIME st;
+	time_t tt = (time_t)0x7FFFFFFF;
+	ConvertTimeTToSystemTime(&st, &tt);
+
+	printf("%4.4d: % 2.2d : % 2.2d: % 2.2d: % 2.2d: % 2.2d\n", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond);
+	int end = clock(); // end time
+	int z = (end - start);
+	int t = (end - start) / CLOCKS_PER_SEC;// transmit to sec
+	cout << " " << z << " " << t << " " << CLOCKS_PER_SEC;
+
+	cin.get();
+	return 0;
+}
+
